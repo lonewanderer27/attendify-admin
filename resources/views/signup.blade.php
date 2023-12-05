e83a3f132661fad98e55667fcfd6550d12b5c481
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-05 09:50:20 +0800

    Add signup page with CSS styling and JavaScript functionality

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,1 +1,58 @@
-<h1>Signup Page</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sign Up</title>
+
+    <!-- Styles -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css'>
+    <link rel="stylesheet" href='{{ asset('css/signup.css') }}'>
+
+</head>
+
+<body>
+    <img src="{{ asset('images/passifi-logo.png') }}" alt="passifi-logo" url="" class="passifi-logo">
+    <cont>
+        <div class="container" id="container">
+            <div class="form-container sign-up-container">
+                <form action="#">
+                    <h1>Create Account</h1>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Confirm Password" />
+                    <button>Sign Up</button>
+                </form>
+            </div>
+            <div class="form-container sign-in-container">
+                <form action="#">
+                    <h1>Sign in</h1>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <a href="#">Forgot your password?</a>
+                    <button>Sign In</button>
+                </form>
+            </div>
+            <div class="overlay-container">
+                <div class="overlay">
+                    <div class="overlay-panel overlay-left">
+                        <h1>Already have an Account?</h1>
+                        <p>To keep connected with us, please log in.</p>
+                        <button class="ghost" id="signIn">Sign In</button>
+                    </div>
+                    <div class="overlay-panel overlay-right">
+                        <h1>Don't have an Account?</h1>
+                        <p>Register now then passifi!</p>
+                        <button class="ghost" id="signUp">Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="{{ asset('js/signup/script.js') }}"></script>
+    </cont>
+</body>
+
+</html>
